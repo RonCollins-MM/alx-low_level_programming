7b2a19b1d5c33348e8031028635d7a3cc3a9f919
Roncollins MM <muriukiron@gmail.com>
2022-04-07 21:49:24 -0700

    C file to calculate size of various data types on local pc

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-	printf("Size of char: %d\n byte(s)", (int)sizeof(char));
-	printf("Size of int: %d\n byte(s)", (int)sizeof(int));
-	printf("Size of long int: %d\n byte(s)", (int)sizeof(long int));
-	printf("Size of long long int: %d\n byte(s)", (int)sizeof(long long int));
-	printf("Size of float: %d\n bytes(s)", (int)sizeof(float));
+	printf("Size of char: %d byte(s)\n", (int)sizeof(char));
+	printf("Size of int: %d byte(s)\n", (int)sizeof(int));
+	printf("Size of long int: %d byte(s)\n", (int)sizeof(long int));
+	printf("Size of long long int: %d byte(s)\n", (int)sizeof(long long int));
+	printf("Size of float: %d bytes(s)\n", (int)sizeof(float));
 	return (0);
 }
