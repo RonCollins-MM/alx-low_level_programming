f39e0f4a73592153d68be989e39d7f2495c19839
Roncollins MM <muriukiron@gmail.com>
2022-04-19 10:04:23 -0700

    Function to print times table of 9, including zero

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * times_table - prints the times table of 9, including 0
+ */
+void times_table(void)
+{
+	int i, j, product;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			product = i * j;
+			_putchar(product + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
