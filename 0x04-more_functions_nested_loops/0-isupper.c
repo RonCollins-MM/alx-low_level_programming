1b86787f38b8eaed73657a124a717e73ff552e5a
Roncollins MM <muriukiron@gmail.com>
2022-04-19 11:59:12 -0700

    Created all files for project. Created function to check if character is uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if character is uppercase
+ * @c: ASCII decimal number of the character to be checked
+ *
+ * Return: 1 if lowercase, 0 if otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+This is yet another directory with more concepts on functions, loops andnested loops.
+
+Refer to individual files and commits for details.
