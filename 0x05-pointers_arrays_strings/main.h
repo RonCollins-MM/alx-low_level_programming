77368cb1b39adb82f87007c7e8d412753d65e9a6
Roncollins MM <muriukiron@gmail.com>
2022-04-21 16:22:40 -0700

    Batch processing

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,5 +6,11 @@ void swap_int(int *, int *);
 int _strlen(char *);
 void _puts(char *);
 void print_rev(char *);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 #endif
