11c052cba92e2fcec21bd2c3796038e5df8b85f9
Roncollins MM <muriukiron@gmail.com>
2022-04-29 15:57:14 +0300

    Function that identifies the first occurrence of a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * _strchr - A function that locates a character in a string
+ * @s: Address of the first character of the string
+ * @c: The character to be looked for
+ *
+ * Return: The address of character if found, NULL if character  not found
+ */
+char *_strchr(char *s, char c)
+{
+	char *frocc = '\0';
+	int i, strlength;
+
+	strlength = _strlen(s);
+	for (i = 0; i < strlength; i++)
+	{
+		if (*(s + i) == c)
+		{
+			frocc = &s[i];
+			break;
+		}
+	}
+	return (frocc);
+}
+
+/**
+ * _strlen - A function that returns the length of a string
+ * @s: Address to the base index of the string
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+	int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		length += 1;
+	}
+
+	return (length);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,5 +3,7 @@
 
 char *_memset(char *, char, unsigned int);
 char *_memcpy(char *, char *, unsigned int);
+int _strlen(char *);
+char *_strchr(char *, char);
 
 #endif
