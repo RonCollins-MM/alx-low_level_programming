ba1b7976f15bd2f65b09cf984fd55455cfe7f6ab
Roncollins MM <muriukiron@gmail.com>
2022-05-01 19:05:33 +0300

    Function to print string in reverse using recursion. Recursion is a mind twister...

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,17 +5,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*(s + 1) == '\0')
+	if (*s)
 	{
-		while (*s != '\0')
-		{
-			_putchar(*s);
-			s--;
-		}
-		return;
-	}
-	else
-	{
-		_print_rev_recursion((s + 1));
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
 }
