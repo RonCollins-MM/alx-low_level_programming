51359fbf24ae21aa3264c3ac1e98016debd4d9fe
Roncollins MM <muriukiron@gmail.com>
2022-05-01 12:31:49 +0300

    Function that prints the sum of numbers along the diagonals of a square matrix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_diagsums - Prints the sum of the numbers along the two 
+ * print_diagsums - Prints the sum of the numbers along the two
  *	diagonals in a square matrix
  * @a: Pointer to the first int in the square matrix
  * @size: Length of the square matrix
@@ -9,7 +9,7 @@
 void print_diagsums(int *a, int size)
 {
 	int s1 = 0, s2 = 0, i, j = 0;
-	
+
 	for (i = 0; i < (size * size); i += ((size + 1) * j))
 	{
 		s1 += *(a + i);
