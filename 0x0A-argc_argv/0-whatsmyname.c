469bf03a40793979de3a970eb30174d34e52f4c4
Roncollins MM <muriukiron@gmail.com>
2022-05-11 14:40:56 +0300

    Created all files. Did the first task 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the program
+ * @argc - A count of the parameters passed in terminal
+ * @argv - The string value of the parameters passed
+ *
+ * Return: 0 (Always success)
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+This directory contains the solutions to the problems on argv and argc in C programming.
+Refer to individual file and commits for details.
