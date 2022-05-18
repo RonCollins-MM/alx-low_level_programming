14cb315b10524003fbd293803f908bcbadbe8018
Roncollins MM <muriukiron@gmail.com>
2022-05-18 16:00:27 +0300

    Created function that initialises a preallocated struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog - A function that initialises a struct
+ * @d: Pointer to the struct to initialise
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of the dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = *name;
+	d->age = age;
+	d->owner = *owner;
+}
