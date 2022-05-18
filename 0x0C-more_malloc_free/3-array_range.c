4a2b7e9295213928c525040c566b210293e3db6c
Roncollins MM <muriukiron@gmail.com>
2022-05-18 10:01:11 +0300

    Function that creates an array containing numbers in a range

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * array_range - Creates an array of integers in a specified range
+ * @min: Lower bound of range
+ * @max: upper bound of range
+ * 
+ * Return: pointer to memory area, NULL if failed.
+ */
+int *array_range(int min, int max)
+{
+	int *dmem, i = 0, elem;
+
+	if (min > max)
+		return (NULL);
+	dmem = malloc((max - min + 1) * sizeof(int));
+
+	if (dmem == NULL)
+		return (NULL);
+	
+	elem = min;
+	while (i <= max - min)
+		dmem[i++] = elem++;
+
+	return(dmem);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,5 +7,6 @@
 void *malloc_checked(unsigned int);
 void *_calloc(unsigned int, unsigned int);
 char *_memset(char *, char, unsigned int);
+int *array_range(int, int);
 
 #endif
