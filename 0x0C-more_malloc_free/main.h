47c09030eed426241ae978e48cee9d3f153d920e
Roncollins MM <muriukiron@gmail.com>
2022-05-18 09:19:33 +0300

    Function that dynamically allocates memory for an array and initializes it to 0

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+#include <stdio.h>
+
 void *malloc_checked(unsigned int);
 void *_calloc(unsigned int, unsigned int);
 char *_memset(char *, char, unsigned int);
