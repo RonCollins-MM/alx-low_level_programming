0b084ef670e45018bf4920240441a7fe79735bdc
Roncollins MM <muriukiron@gmail.com>
2022-05-18 08:25:38 +0300

    Created all files. Added function that allocates memory dynamically, but terminates with exit() on fail

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * malloc_checked - Dynamically allocates memory of size b.
+ *	Exits normally if failed.
+ * @b: Size if memory
+ *
+ * Return: Pointer to memory allocated.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *dmem;
+
+	dmem = malloc(b);
+	if (dmem == NULL)
+		exit(98);
+
+	return (dmem);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,2 @@
+This directory contains solutions to problems from the ALX intranet that aim to reinforce the concept of dynamic memory allocation as well as exit() function.
+Refer to individual files and commits for more details.
